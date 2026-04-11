aaf7cc59536182c7f49afd9e3c52973b12b83e2e
Vaibhav Prakash <vaibhavprakash180@gmail.com>
2026-04-11 21:54:58 +0530

    CORS check

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://prepify-ai-zeta.vercel.app/",
+    origin: "https://prepify-ai-zeta.vercel.app",
     credentials: true
 }))
 
